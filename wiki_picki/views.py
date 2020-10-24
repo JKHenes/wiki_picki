db058474370f7942b67a68fe63345b97f628ef1d
Max Warren <maxwarren@live.co.uk>
2020-10-24 18:13:38 +0100

    added a search form

--- wiki_picki/views.py
+++ wiki_picki/views.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, session, redirect
+from flask import Flask, render_template, session, redirect, url_for, request
 from wiki_picki import app
 
 @app.route('/')
@@ -8,6 +8,15 @@ def welcome():
 @app.route('/search', methods=['GET', 'POST'])
 def decision(name=None):
     if request.method == 'POST':
-        if
+        if 'searches' in session:
+            session['searches'].append(0, [request.form['search'], request.form['website']])
+        else:
+            session['searches'] = [[request.form['search'], request.form['website']]]
+        return redirect(url_for('result'))
     return render_template('searchMenu.jinja', name=name)
 
+@app.route('/result')
+def result():
+    search = session['searches'][0][0]
+    website = session['searches'][0][1]
+    return render_template('searchresult.jinja', search=search, website=website)
