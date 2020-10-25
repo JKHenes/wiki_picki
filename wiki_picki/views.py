d80eb31b610381580bfb9fb33544f1c40f65219a
Max Warren <maxwarren@live.co.uk>
2020-10-25 01:41:34 +0000

    Added a selection

--- wiki_picki/views.py
+++ wiki_picki/views.py
@@ -3,6 +3,8 @@ from subprocess import call
 from wiki_picki import *
 from wiki_picki.sonicoc import *
 
+WEBSITES={"Sausage":"sausage.fandom.com", "Vtubers": "virtualyoutuber.fandom.com", "Sonic fan characters":"sonicfanchara.fandom.com", "Super powers":"powerlisting.fandom.com"}
+
 @app.route('/')
 def welcome():
     return render_template('welcomeScreen.jinja')
@@ -10,10 +12,12 @@ def welcome():
 @app.route('/search', methods=['GET', 'POST'])
 def decision(name=None):
     if request.method == 'POST':
+        session.clear()
+        web_addr = WEBSITES[request.form['website']]
         if 'searches' in session:
-            session['searches'].append( [request.form['search'], request.form['website']])
+            session['searches'].append( [request.form['search'], web_addr])
         else:
-            session['searches'] = [[request.form['search'], request.form['website']]]
+            session['searches'] = [[request.form['search'], web_addr]]
         return redirect(url_for('result'))
     return render_template('searchMenu.jinja', name=name)
 
@@ -21,6 +25,7 @@ def decision(name=None):
 def result():
     search = session['searches'][0][0]
     website = session['searches'][0][1]
+    print("attempting" + search + "   " + website)
     data=get_info(search,website)
     
     return render_template('searchresult.jinja', title=data[0],url=data[1],text=data[2])
