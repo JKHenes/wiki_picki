7f6ace000ef93aaa30471260059ba2269fe58b62
Jacqui H <jacqui.henes@gmail.com>
2020-10-25 01:47:05 +0000

    IT WORKS???

--- wiki_picki/sonicoc.py
+++ wiki_picki/sonicoc.py
@@ -1,4 +1,4 @@
-import sys, http.client, urllib.request, urllib.parse, urllib.error, json
+import sys, http.client, urllib.request, urllib.parse, urllib.error, json, re, random
 
 from pprint import pprint
 
@@ -32,9 +32,29 @@ def do_search(query, website):
 
     return search_result
 
+def get_random_chara(): #THIS IS SPECIFIC TO THE SONIC WEBSITE
+    id = [149610, 171415,20601,84540,172886,174980,146592,145006,3177,175147,3689,36402,175143,13739,148487] 
+    return random.choice(id)
+
+def sonic_name_matcher(result): # ALSO SPECIFIC TO SONIC
+    name_rx = "\\w+ [tT]he \\w+"
+    matcher = None
+    for items in result.get("items"):
+        matcher = re.match(name_rx,str(items.get("title")))
+        if matcher:
+            result = items
+            break
+
+    # if you cant find a match get a random article
+    if matcher is not None:
+        article_id = result.get("id")
+    else:
+        article_id = get_random_chara()
+
+    return result, article_id
+
 # Searches a website, and tries to find the title, url, and content of the first article it finds. 
 def get_info(query,website):
-
 
     # Make sure that the query can actually parsed by the get request
     query = urllib.parse.quote_plus(query)
@@ -42,24 +62,23 @@ def get_info(query,website):
     # Gets the first search result from a query
     search_data = do_search(query, website)
 
-    if search_data is None:
-        search_data = do_search(query[0:3], website)
-        if search_data is None:
-            print("No data for you wahh") # replace this with a random article?
-            sys.exit()
-
-    search_data = search_data.get("items")[0]
-
-    article_id = search_data.get("id")
-    title = search_data.get("title")
-    url = search_data.get("url")
-
+    if website == "sonicfanchara.fandom.com": # Specific Sonic character parsing
+        search_data,article_id = sonic_name_matcher(search_data)
+    else:                                     # Generic approach: will just select the first article 
+        search_data = search_data.get("items")[0]
+        article_id = search_data.get("id")
+    
     # Shouuuuld be fine? if there was no data it would've exited out before.
     # Gets the specific article from an article
     article_data = get_url(website, '/api/v1/Articles/AsSimpleJson?id=' + str(article_id))
     article_data = article_data.decode("utf-8")
     article_data = json.loads(article_data)
 
+    # Get title and URL
+    title = article_data.get("sections")[0].get("title")
+    url = "https://"+ website + "/" + title.replace(" ", "_")
+
+    # Get some text content
     text = ""
     # Print out text content of wiki, goes through each 'section'
     for sections in article_data.get("sections"):
@@ -70,9 +89,16 @@ def get_info(query,website):
         except IndexError as e:
             pass
 
+    # Get image source if it can find one, None if it can't
+    image =  None
+    try:
+        image =  article_data.get("sections")[0].get("images")[0].get("src")
+    except IndexError as e:
+        image = None
+
     # Debug prints
     #print(title)
     #print(url)
     #print(text[:-1])
 
-    return (title, url, text[:-1])
+    return (title, url, text[:-1], image)
