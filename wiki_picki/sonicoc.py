79e2e49af642c18755c8f73c64de0ec5b7569876
Jacqui H <jacqui.henes@gmail.com>
2020-10-25 03:47:15 +0000

    we got vtubers now B)

--- wiki_picki/sonicoc.py
+++ wiki_picki/sonicoc.py
@@ -53,6 +53,17 @@ def sonic_name_matcher(result): # ALSO SPECIFIC TO SONIC
 
     return result, article_id
 
+def Vtuber(website):
+    top_list = get_url(website, "api/v1/Articles/Top")
+    top_list = top_list.decode("utf-8")
+    top_list = json.loads(top_list)
+    best_girl =  random.choice(top_list.get("items")).get("id")
+    best_girl_results = get_url(website, "api/v1/Articles/Details?ids=" + str(best_girl))
+    best_girl_results= best_girl_results.decode("utf-8")
+    best_girl_results= json.loads(best_girl_results)
+
+    return best_girl_results, best_girl
+
 # Searches a website, and tries to find the title, url, and content of the first article it finds. 
 def get_info(query,website):
 
@@ -62,45 +73,59 @@ def get_info(query,website):
     # Gets the first search result from a query
     search_data = do_search(query, website)
 
+    Vtuber_f = False    
+
     if website == "sonicfanchara.fandom.com": # Specific Sonic character parsing
         search_data,article_id = sonic_name_matcher(search_data)
+    elif website == "virtualyoutuber.fandom.com":
+        search_data, article_id = Vtuber(website)
+        Vtuber_f = True
     else:                                     # Generic approach: will just select the first article 
         search_data = search_data.get("items")[0]
         article_id = search_data.get("id")
     
     # Shouuuuld be fine? if there was no data it would've exited out before.
     # Gets the specific article from an article
-    article_data = get_url(website, '/api/v1/Articles/AsSimpleJson?id=' + str(article_id))
-    article_data = article_data.decode("utf-8")
-    article_data = json.loads(article_data)
+    if not Vtuber_f:
+        article_data = get_url(website, '/api/v1/Articles/AsSimpleJson?id=' + str(article_id))
+        article_data = article_data.decode("utf-8")
+        article_data = json.loads(article_data)
 
-    # Get title and URL
-    title = article_data.get("sections")[0].get("title")
-    url = "https://"+ website + "/" + title.replace(" ", "_")
+        # Get title and URL
+        title = article_data.get("sections")[0].get("title")
+        url = "https://"+ website + "/" + title.replace(" ", "_")
 
-    # Get some text content
-    text = ""
-    # Print out text content of wiki, goes through each 'section'
-    for sections in article_data.get("sections"):
+        # Get some text content
+        text = ""
+        # Print out text content of wiki, goes through each 'section'
+        for sections in article_data.get("sections"):
+            try:
+                fragment_title = sections. get("content")[0].get("title")
+                fragment = sections.get("content")[0].get("text")
+                if fragment is not None:
+                    if fragment_title == None:
+                        fragment_title = ""
+                    else:
+                        fragment_title += ": "
+                    text += fragment_title + fragment + "\n"
+            except IndexError as e:
+                pass
+
+        # Get image source if it can find one, None if it can't
+        image =  None
         try:
-            fragment_title = sections. get("content")[0].get("title")
-            fragment = sections.get("content")[0].get("text")
-            if fragment is not None:
-                if fragment_title == None:
-                    fragment_title = ""
-                else:
-                    fragment_title += ": "
-                text += fragment_title + fragment + "\n"
+            image =  article_data.get("sections")[0].get("images")[0].get("src")
         except IndexError as e:
-            pass
+            image = None
 
-    # Get image source if it can find one, None if it can't
-    image =  None
-    try:
-        image =  article_data.get("sections")[0].get("images")[0].get("src")
-    except IndexError as e:
-        image = None
+    else:
+        article_data = search_data.get("items").get(str(article_id))
+        title = article_data.get("title")
+        url = "https://"+ website +article_data.get("url")
+        text = article_data.get("abstract")
+        image = article_data.get("thumbnail")[:-61]
 
+    
     #print(text)
     # Debug prints
     #print(title)
